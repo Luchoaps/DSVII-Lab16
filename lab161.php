1aef1d9159b801f7080000a845535e0a3451fb43
Luchoaps <112591814+Luchoaps@users.noreply.github.com>
2022-11-11 18:35:57 -0500

    Laboratorio 16

---
+++ lab161.php
@@ -0,0 +1,39 @@
+<html lang="es">
+<head>
+    <title>Laboratorio 16.1</title>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+    <h1>Consulta de Servicio Web de Conversión de Temperatura</h1>
+    <form name="FormConv" method="post" action="Lab161.php">
+        <br>
+        Convertir desde 
+        <select name="temp" id="">
+            <option value="Ctof" selected>°C a °F</option>
+            <option value="FtoC">°F a °C</option>
+        </select>el valor
+        <input type="number" name="valor" id="" step="Any" required>
+        <input name="Convertir" id="" value="Convertir" type="submit">
+    </form>
+    <?php
+        $servicio="https://www.w3schools.com/xml/tempconvert.asmx?wsdl";
+        $parametros=array();
+        if(array_key_exists('Convertir', $_POST)){
+            $valor=$_POST['valor'];
+            $temp=$_POST['temp'];
+        
+            if ($temp=="CtoF"){
+                $parametros['Celsius']=$valor;
+                $cliente=new SoapClient($servicio, $parametros);
+                $resultObj=$cliente->CelsiusToFahrenheit($parametros);
+                $resultado=$resultObj->CelsiusToFahrenheitResult;
+            }else{
+                $parametros['Fahrenheit']=$valor;
+                $cliente=new SoapClient($servicio, $parametros);
+                $resultObj=$cliente->FahrenheitToCelsius($parametros);
+                $resultado=$resultObj->FahrenheitToCelsiusResult;
+            }print ("<br>La temperatura $valor". substr($temp,0,1)." es igual a: . $resultado" . substr($temp,3,1));
+        }
+    ?>
+</body>
+</html>
